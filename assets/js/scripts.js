dc94b9d03f58b5b1211eb03ef10c16874d95a29c
James Sabel <jnsabel@gmail.com>
2024-03-27 20:28:38 -0300

    Removed inline script and used jQuery listeners for code optimization.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,18 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    $("#response-select").on("change", function () {
+        var selectedCode = $(this).val();
+        var methodName = $(this).data("methodname");
+        var endpointName = $(this).data("endpointname");
+
+        showResponseContent(selectedCode, methodName, endpointName);
+    });
 });
 
 function showResponseContent(code, method, endpoint) {
-
     $("#response-select").find("option:selected").each(function () {
         if (code) {
             $("." + method + "-" + endpoint).show();
             $("." + method + "-" + endpoint).not("." + code + "-description").hide();
         }
     });
-
 }
